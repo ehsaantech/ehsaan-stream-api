5f8d75fb4b573700d33b51e457c18fee95c48455
Nehal409 <nehalnasir619@gmail.com>
2023-02-15 16:20:16 +0500

    initial commit

--- stream-api/src/dto/create-channel.dto.ts
+++ stream-api/src/dto/create-channel.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateChannelDto {
+  @IsNotEmpty()
   name: string;
+  
+  @IsNotEmpty()
   thumbnail: string;
+
+  @IsNotEmpty()
   description: string;
 }
